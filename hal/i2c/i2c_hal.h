d502cc9552cf60c2ea4d0fd1373c04386837fb79
SanchD <sanchithadias@Sanchithas-MacBook-Air.local>
2023-11-12 18:31:16 +0800

    feat(Init):Initial folder structure of LIS3MDL test application
    Notes: Integrated generic driver from the supplier as submodule

---
+++ hal/i2c/i2c_hal.h
@@ -0,0 +1,25 @@
+/*****************************************************************
+ * @file    i2c_hal.h
+ * @author  Sanchitha Dias
+ * @date    12/11/2024
+ * @brief   Contains all the interfaces of I2C communication of
+ *          the Magnetometer Test Application.
+******************************************************************/
+#ifndef I2C_HAL_H
+#define I2C_HAL_H
+
+#include "Board.h"
+#include "stdint.h"
+
+uint8_t i2c_read(uint8_t bus_address, uint8_t register_address, uint8_t *buffer,
+uint16_t length);
+
+uint8_t i2c_write(uint8_t bus_address, uint8_t register_address, uint8_t *data,
+uint16_t length);
+
+#endif
+/******************************************************************
+*********                       EOF                       *********
+*******************************************************************
+********* Dev. by Sanchitha Dias (sanchithadias@gmail.com)*********
+******************************************************************/
